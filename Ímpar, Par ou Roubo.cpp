06fa5bea1c50d2d48f54063ca776977a6a033d39
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2022-11-05 11:39:18 -0300

    Update Ímpar, Par ou Roubo.cpp

--- Ímpar, Par ou Roubo.cpp
+++ Ímpar, Par ou Roubo.cpp
@@ -2,47 +2,46 @@
 #include <locale.h>
 
 int main() {
-	setlocale(LC_ALL, "Portuguese_Brazil"); 
-    int p, j1, j2, r, a, soma, ganha;
+    setlocale(LC_ALL, "Portuguese_Brazil"); 
     
-	printf("Qual foi a escolha do Jogador 1? \nDigite 1 para PAR ou 0 para ÕMPAR: ");
+    int p, j1, j2, r, a, soma, ganha;
+	
+    printf("Qual jogador vai ganhar?");
+    
+    printf("Qual foi a escolha do Jogador 1? \nDigite 1 para PAR ou 0 para √çMPAR: ");
     scanf("%d", &p);
     
     printf("\n");
-    printf("Qual foi o n˙mero escolhido pelo jogador 1? ");
+    printf("Qual foi o n√∫mero escolhido pelo jogador 1? ");
     scanf("%d", &j1);
     
-    printf("Qual foi o n˙mero escolhido pelo jogador 2? ");
+    printf("Qual foi o n√∫mero escolhido pelo jogador 2? ");
     scanf("%d", &j2);
     
     printf("\n");
-    printf("O jogador 1 roubou? \nDigite 1 para SIM ou 0 para N√O: ");
+    printf("O jogador 1 roubou? \nDigite 1 para SIM ou 0 para N√ÉO: ");
     scanf("%d", &r);
     
     printf("\n");
-    printf("O jogador 2 acusou o jogador 1 de roubo? \nDigite 1 se ELE ACUSOU ou 0 se ele N√O ACUSOU: ");
+    printf("O jogador 2 acusou o jogador 1 de roubo? \nDigite 1 se ELE ACUSOU ou 0 se ele N√ÉO ACUSOU: ");
     scanf("%d", &a);
     
     soma = j1 + j2;
     
     if (soma%2==0 && p==1){
         ganha = 1;
-	} else{
+    } else{
     	ganha = 2;
-	}
-    
+    }
     if (p==0 && soma%2==1){
         ganha = 1;
-    }else{
+    } else{
         ganha = 2;
     }
-
     if (r==1 && a==1){
         ganha = 2;
     }
-    
-    if (r==1 && a==0){
-        
+    if (r==1 && a==0){   
         ganha = 1;
     }
     
