323cfc7bfdd6ddbd57b4d7e272eb88d4603bff6f
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2022-11-05 11:19:13 -0300

    Update Fuso Horário.cpp

--- Fuso Horário.cpp
+++ Fuso Horário.cpp
@@ -2,29 +2,29 @@
 #include <locale.h>
 
 int main() {
-	setlocale(LC_ALL, "Portuguese_Brazil");
+    setlocale(LC_ALL, "Portuguese_Brazil");
 	
     int saida, tempo, fuso;
     
-    printf("Saiba qual ser· a hora quando chegar no seu destino.\n\n");
+    printf("Saiba qual ser√° a hora quando chegar no seu destino.\n\n");
     
-    printf("Qual foi a hora da sua saÌda? ");
+    printf("Qual foi a hora da sua sa√≠da? ");
     scanf("%d", &saida);
     
-    printf("Quanto tempo de duraÁ„o da sua viagem? ");
+    printf("Quanto tempo de dura√ß√£o da sua viagem? ");
     scanf("%d", &tempo);
     
-    printf("Qual o fuso hor·rio no seu local de destino? ");
+    printf("Qual o fuso hor√°rio no seu local de destino? ");
     scanf("%d", &fuso);
     
     if(saida==0){
     	saida = 24;
     } else {
 	saida = ((saida + tempo + fuso) % 24);
-	}
-	
-	printf("\n");
-    printf("A hora atual no seu local de destino ser· ·s: %d horas\n", saida);    
+    }
+    
+    printf("\n");
+    printf("A hora atual no seu local de destino ser√° √°s: %d horas\n", saida);    
     
     return 0;
 }
