366407a290bb7a13e621376e1d2c134a2a64a506
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2022-11-06 10:41:52 -0300

    Update Fórmula de Bhaskara.cpp

--- Fórmula de Bhaskara.cpp
+++ Fórmula de Bhaskara.cpp
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
  
 int main() {
-    setlocale(LC_ALL, "Portuguese_Brazil");
 	
     double A, B, C, Delta;
     
-    printf("Programa para calcular a fórmula de Bhaskara.\n\n");
+    printf("Programa para calcular a formula de Bhaskara.\n\n");
     
-    printf("Digite o primeiro número da equação: ");
+    printf("Digite o primeiro numero da equacao: ");
     scanf("%lf", &A);
     
-    printf("Digite o segundo número da equação: ");
+    printf("Digite o segundo numero da equacao: ");
     scanf("%lf", &B);
     
-    printf("Digite o terceiro número da equação: ");
+    printf("Digite o terceiro numero da equacao: ");
     scanf("%lf", &C);
     
     Delta = (B*B - 4*A*C);
     
     printf("\n");
     
-    if(Delta>=0 && A!=0){	
+    if(Delta>=0 && A!=0){
     	printf("X LINHA = %.5lf\n", ((-B) + sqrt(Delta)) / (2*A));
-        printf("X DUAS LINHAS = %.5lf\n", ((-B) - sqrt(Delta)) / (2*A));    
+        printf("X DUAS LINHAS = %.5lf\n", ((-B) - sqrt(Delta)) / (2*A));
     } else{
-    	printf("Ímpossivel calcular o delta\n");   
+    	printf("Impossivel calcular o delta\n");   
     }
     
     return 0;
