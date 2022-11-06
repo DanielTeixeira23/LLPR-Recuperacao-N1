e51d35991cc81de422e4e71ee5df2081d8a68e1a
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2022-11-06 11:10:18 -0300

    Update Sort Simples.cpp

--- Sort Simples.cpp
+++ Sort Simples.cpp
@@ -1,48 +1,46 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main(){
-	setlocale(LC_ALL, "Portuguese_Brazil");
+
+    int a,b,c,x,y,z,teste;
 	
-	int a,b,c,x,y,z,teste;
+    printf("Sort Simples\n\n");
 	
-	printf("Sort Simples\n\n");
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &a);
 	
-	printf("Informe o primeiro numero: ");
-	scanf("%d", &a);
+    printf("Informe o segundo numero: ");
+    scanf("%d", &b);
 	
-	printf("Informe o segundo numero: ");
-	scanf("%d", &b);
+    printf("Informe o terceiro numero: ");
+    scanf("%d", &c);
 	
-	printf("Informe o terceiro numero: ");
-	scanf("%d", &c);
+    x = a;
+    y = b;
+    z = c;
+
+    if(a>b){	
+	teste = a;
+	a = b;
+	b = teste;
+    }
+    if(a>c){		
+	teste = a;
+	a = c;
+	c = teste;
+    }
+    if(b>c){	
+	teste = b;
+	b = c;
+	c = teste;
+    }
+    printf("\n");
 	
-	x = a;
-	y = b;
-	z = c;
+    printf("Numeros em ordem crescente: \n");
+    printf("%d\n%d\n%d\n\n",a,b,c);
 	
-	if(a>b){
-	   teste = a;
-	   a = b;
-       	   b = teste;
-	}else{
-	  teste = a;
-	  a = c;
-	  c = teste;
-	}
-	if(b>c){
-	   teste = b;
-	   b = c;
-	   c = teste;
-	}
+    printf("Numeros na sequencia que foram digitados: \n");
+    printf("%d\n%d\n%d\n",x,y,z);
 	
-	printf("\n");
-	
-	printf("Numeros em ordem crescente: \n");
-	printf("%d\n%d\n%d\n\n",a,b,c);
-	
-	printf("Numeros na sequencia que foram digitados: \n");
-	printf("%d\n%d\n%d\n",x,y,z);
-	
-	return 0;
+    return 0;
 }
